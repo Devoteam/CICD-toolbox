08970e8e05d72b779aeefc077a2262442a5cc763
Mark Sibering <46484069+netcicd@users.noreply.github.com>
2020-12-04 11:16:31 +0100

    Delete rawRepositories.groovy

--- nexus/rawRepositories.groovy
+++
@@ -1,7 +0,0 @@
-// create a new blob store dedicated to usage with raw repositories
-def rawStore = blobStore.createFileBlobStore('raw', 'raw')
-
-// and create a first raw hosted repository for documentation using the new blob store
-repository.createRawHosted('NetCICD Test reports', rawStore.name)
-
-log.info('Created repository for ROBOT testreports successfully')
